181d5dc0aa62e84914e4eaf6caf4595ac954ac22
DevPoint Labs <DPL1@devpointsimac3.domain.actdsltmp>
2014-05-30 15:19:19 -0600

    added hashes.rb

---
+++ hashes.rb
@@ -0,0 +1,8 @@
+about_me = {name: "Tyson", birthday: "June, 12 1989", hometown: "Provo", favoritefood: "Japanese", favoritemovie: "Dumb and Dumber", favoritehobby: "Hiking"}
+
+puts about_me.values.inspect
+
+puts about_me.has_key?('name')
+puts about_me.has_value?('Tyson')
+puts about_me.shift
+puts about_me.length
