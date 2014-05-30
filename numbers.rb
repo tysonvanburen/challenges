53702c9bf134e503ce1aa11af1a5c9201f6326ee
DevPoint Labs <DPL1@devpointsimac3.domain.actdsltmp>
2014-05-30 14:20:43 -0600

    added numbers.rb

---
+++ numbers.rb
@@ -0,0 +1,28 @@
+puts "Lets do some math:"
+
+puts "25 + 30 / 6 is "
+puts 25 + 30 / 6
+
+
+puts "100 - 25 * 3 / 4 is "
+puts 100 - 25 * 3 / 4
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+puts 3 + 2 < 5 - 7
+
+puts "Is 5 greater than -2"
+puts 5 > -2
+
+puts "Is 5 greator or equal to -2?"
+5 >= -2
+
+puts "Is 5 less than or equal to -2?"
+5 <= -2
+
+number1 = 10
+number2 = 5
+number3 = 1
+
+number1.to_s
+number2.times
+number3.to_i
